0c26b4501b722a0fa83a7689aaf671525c484867
xgyu <82879845+xgyu1024@users.noreply.github.com>
2023-02-24 17:52:41 +0800

    feat:兼容vue2、3，添加types，删除demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "vue-eslint-parser",
-  plugins: ["@typescript-eslint"],
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	plugins: ["@typescript-eslint"],
+	parserOptions: {
+		parser: "@typescript-eslint/parser",
+	},
+	rules: {
+		semi: ["error", "tab"],
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+	},
 };
