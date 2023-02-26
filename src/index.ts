300fe22c6e10ba9e89d489fa61a14007b8540e98
xgyu <82879845+xgyu1024@users.noreply.github.com>
2023-02-26 18:12:04 +0800

    feat:去插件化

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * 主题键值对
+ */
+let themes = {};
+
+/**
+ * 注册主题
+ * @param themes
+ */
+export function registerThemes(themes: any) {
+	themes = themes;
+}
+
+/**
+ * 设置主题
+ * @param themeName 主题键名
+ */
+export function setTheme(themeName: string) {
+	const theme = (themes as any)[themeName];
+
+	// 为主题中所有的变量进行赋值
+	for (const key in theme) {
+		const v = theme[key];
+
+		if (typeof v === "string") {
+			document.documentElement.style.setProperty(key, v);
+		}
+	}
+}
